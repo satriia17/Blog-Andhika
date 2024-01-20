4d8f8bda0714d3e81f0fb5d83a3112d042951ee4
satriia17 <andhika.raffly06@gmail.com>
2024-01-20 15:45:12 +0700

    Create header with flowbite react

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div>Hello World!</div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sing-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
