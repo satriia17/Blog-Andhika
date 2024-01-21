bac67a484b20905cbea9e856497312f4df3089ca
satriia17 <andhika.raffly06@gmail.com>
2024-01-21 18:27:15 +0700

    [Doc] Added docummentation

--- Backend/controllers/auth.control.js
+++ Backend/controllers/auth.control.js
@@ -2,7 +2,6 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/errorhandle.js";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -36,23 +35,24 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password || email === "" || password === "") {
-    next(errorHandler(400, "Please provide email and password"));
+    next(errorHandler(400, "Please provide email and password")); //cek apakah email dan password sudah terisi
   }
-  try{
-    const validUser = await User.findOne({ email });
+  try {
+    const validUser = await User.findOne({ email }); //menunggu untuk validasi email
     if (!validUser) {
-      return next(errorHandler(404, "User Not Found"));
+      return next(errorHandler(404, "Invalid Password")); //jika email tidak ditemukan maka error
     }
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password); //jika email ditemukan maka validasi password yang sudah di hash menggunakan bycrypt
     if (!validPassword) {
-      return next(errorHandler(404, "User Not Found"));
+      return next(errorHandler(404, "Invalid Password")); //jika password tidak sesuai maka error
     }
-    const token = jwt.sign(
-      { id: validUser._id }, process.env.JWT_SECRET);
-    const { password: hash, ...others } = validUser._doc;
-    res.status(200).cookie('access_token', token, {httpOnly: true}).json(others); 
-  }catch{
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); //menggunakan Json Web Token untuk membuat token
+    const { password: hash, ...others } = validUser._doc; //menghilangkan data password pada user
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(others); //Memberikan token kedalam cookie supaya user tidak perlu login kembali
+  } catch {
     next(errorHandler(500, "Internal server error"));
-  }
-
-}
+  } 
+};
