5c554b3b94872bf5d5cc86b7c8bd3745dd7a9da9
satriia17 <andhika.raffly06@gmail.com>
2024-01-29 18:23:14 +0700

    [Features] added show post for admin

--- Backend/routes/post.route.js
+++ Backend/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createPost } from "../controllers/createPost.control.js";
+import { createPost, getPost } from "../controllers/post.control.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createPost);
+router.get("/getPost", getPost)
 
 export default router
